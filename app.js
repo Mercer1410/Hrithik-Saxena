c1bb4c5fa96b17a8317d2e7aa0d0cd1ced776f28
Mercer1410 <hs7582@srmist.edu.in>
2021-04-11 12:28:10 +0530

    disable inspect

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+
 jQuery(document).ready(function() {
 
   var mouseX = 0, mouseY = 0;
